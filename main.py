d7d8281281bed1edb307af1f9db810e216ab06a0
Taras Kataryna <tarikkataryna1999@gmail.com>
2019-12-15 19:17:14 +0200

    Add some initial code

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-import application_settings
+import application_settings as app_set
+from telegran_bot import TelegramBot
+
+if __name__ == '__main__':
+        TelegramBot(app_set.TELEGRAM_BOT_TOKEN).listen()
--- telegran_bot.py
+++ telegran_bot.py
@@ -1,11 +1,19 @@
-
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
 
 class TelegramBot:
-    def __init__(self):
+    def __init__(self, telegram_bot_key):
+        self.updater = Updater(token=telegram_bot_key)
+        self.dispatcher = self.updater.dispatcher
         pass
 
     def listen(self):
+        self.add_handlers()
+        self.updater.start_polling()
         pass
 
-    def message_handler(self):
+    def message_handler(self, bot, update):
+        print(update.effective_message.text)
         pass
+
+    def add_handlers(self):
+        self.dispatcher.add_handler(MessageHandler(Filters.text, self.message_handler))
